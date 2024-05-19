ec3608e66d145de6104ba7ead3f16d07ad300871
Vladimir Sobetskii <Sobetskiy.81@gmail.com>
2024-05-19 20:19:16 +0300

    newcommit

--- NewsPaper/news/models.py
+++ NewsPaper/news/models.py
@@ -61,6 +61,9 @@ class Post(models.Model):
     def preview(self):
         return f"{self.text[:124]}..."
 
+    def __str__(self):
+        return f'{self.header}...'
+
 
 class PostCategory(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
